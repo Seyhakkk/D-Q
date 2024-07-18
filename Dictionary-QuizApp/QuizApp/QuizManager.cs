89bdc6dee46131096e1d5d49b7e433ed80a84c36
Seyhakkk <90004742+Seyhakkk@users.noreply.github.com>
2024-07-18 09:49:19 +0700

    Update QuizManager.cs

--- Dictionary-QuizApp/QuizApp/QuizManager.cs
+++ Dictionary-QuizApp/QuizApp/QuizManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -47,11 +47,11 @@ namespace QuizApp
                 return;
             }
 
-            Console.WriteLine($"Welcome to the Quiz Application, {currentUser.Username}!");
+            Console.WriteLine($" <Welcome to Quiz Application> , {currentUser.Username}!");
 
             while (true)
             {
-                int input = IntegerInput("Menu:\n1. Start Quiz\n2. View Previous Results\n3. Manage Question(Add, Update, Delete)\n4. Delete All Question\n5. Change Setting\n0. Exit\nEnter your choice: ");
+                int input = IntegerInput("Menu:\n1. Start Quiz\n2. View Previous Results\n3. Manage Question(Add,Update,Delete)\n4. Delete All Question\n5. Change Setting\n0. Exit\nEnter your choice: ");
                 Console.Clear();
 
                 switch (input)
@@ -128,23 +128,6 @@ namespace QuizApp
         }
 
 
-        // Function store username & password 
-        /*    private bool Login()
-            {
-                string Username = "Somnang";
-                int Password = 21102000;
-                string inputUsername = InputString("Enter login username: ");
-                int inputPassword = IntegerInput("Enter login password: ");
-
-                if (Username == inputUsername && Password == inputPassword)
-                {
-                    // currentUser = user;
-                    Console.WriteLine("Login successful!");
-                    return true;
-                }
-                Console.WriteLine("Login failed. Invalid username or password.");
-                return false;
-            }*/
 
         private bool Register()
         {
@@ -202,49 +185,30 @@ namespace QuizApp
         private void InitializeQuestions()
         {
             // Add some example questions to each category
-            easyCategory.AddQuestion(new QuizQuestion("1. Which of the following are programming languages?\n", new List<string> { "Python", "HTML", "Java", "CSS" }, new List<int> { 0, 2 }));
-            easyCategory.AddQuestion(new QuizQuestion("2. Which of the following are elements on the periodic table?\n", new List<string> { "Oxygen", "Hydrogen", "Water", "Carbon" }, new List<int> { 0, 3 }));
+            easyCategory.AddQuestion(new QuizQuestion("1. Which of the following are elements on the periodic table?\n", new List<string> { "Oxygen", "Hydrogen", "Water", "Carbon" }, new List<int> { 0, 3 }));
+            easyCategory.AddQuestion(new QuizQuestion("2. Which of the following are programming languages?\n", new List<string> { "Python", "HTML", "Java", "CSS" }, new List<int> { 0, 2 }));
             easyCategory.AddQuestion(new QuizQuestion("3. Which of these are programming concepts?\n", new List<string> { "Loop", "Variable", "HTML", "Array" }, new List<int> { 0, 3 }));
             easyCategory.AddQuestion(new QuizQuestion("4. Which of these are social media platforms?\n", new List<string> { "Facebook", "Google", "Instagram", "Whatapp" }, new List<int> { 0, 2 }));
             easyCategory.AddQuestion(new QuizQuestion("5. Which of these are JavaScript frameworks?\n", new List<string> { "Angular", "React", "Django", "Vue" }, new List<int> { 0, 1     }));
-            easyCategory.AddQuestion(new QuizQuestion("6. Which of these are official languages of the United Nations?\n", new List<string> { "English", "Russian", "German", "Spanish" }, new List<int> { 0, 3 }));
-            easyCategory.AddQuestion(new QuizQuestion("7. Which of the following are fundamental forces in physics?\n", new List<string> { "Gravity", "Electromagnetism", "Water", "Carbon" }, new List<int> { 0, 3 }));
-            easyCategory.AddQuestion(new QuizQuestion("8. Which of these are types of clouds?\n", new List<string> { "Cirrus", "Cumulus", "Stratus", "Nimbus" }, new List<int> { 0, 2 }));
-            easyCategory.AddQuestion(new QuizQuestion("9. Which of the following are operating systems?\n", new List<string> { "Windows", "Android", "iPhone", "MacOS" }, new List<int> { 0, 3 }));
-            easyCategory.AddQuestion(new QuizQuestion("10. Which of these are prime numbers?\n", new List<string> { "4", "7", "11", "15" }, new List<int> { 1, 2 }));
-
+          
             mediumCategory.AddQuestion(new QuizQuestion("1. Which of the following are planets in our solar system?\n", new List<string> { "Pluto", "Mars", "Venus", "Alpha Centauri" }, new List<int> { 1, 2 }));
             mediumCategory.AddQuestion(new QuizQuestion("2. Which of these are famous painters?\n", new List<string> { "Leonardo da Vinci", "Albert Einstein", "Vincent van Gogh", "Nikola Tesla" }, new List<int> { 0, 2 }));
             mediumCategory.AddQuestion(new QuizQuestion("3. Which of the following are types of data structures?\n", new List<string> { "Array", "Linked List", "PDF", "CSS" }, new List<int> { 0, 1 }));
             mediumCategory.AddQuestion(new QuizQuestion("4. Which of the following are commonly used databases in web development?\n", new List<string> { "MongoDB", "SQLite", "Photoshop", "Illustrator" }, new List<int> { 0, 1 }));
             mediumCategory.AddQuestion(new QuizQuestion("5. Which of the following are examples of web development frameworks?\n", new List<string> { "React", "Angular", "Excel", "WordPress" }, new List<int> { 0, 2 }));
-            mediumCategory.AddQuestion(new QuizQuestion("6. Which of these are types of databases?\n", new List<string> { "MySQL", "PostgreSQL", "JSON", "XML" }, new List<int> { 0, 1 }));
-            mediumCategory.AddQuestion(new QuizQuestion("7. Which of the following are used to connect to the internet?\n", new List<string> { "Moderm", "Router", "Printer", "Scanner" }, new List<int> { 0, 1 }));
-            mediumCategory.AddQuestion(new QuizQuestion("8. Which of the following are examples of system software?\n", new List<string> { "Operating System", "Device Drivers", "Microsoft Word", "Adobe Acrobat" }, new List<int> { 0, 1 }));
-            mediumCategory.AddQuestion(new QuizQuestion("9. Which of these are types of malware?\n", new List<string> { "Virus", "Trojan Horse", "HTML", "URL" }, new List<int> { 0, 2 }));
-            mediumCategory.AddQuestion(new QuizQuestion("10 Which of the following are examples of cloud storage services?\n", new List<string> { "Google Drive", "Dropbox", "Window Explorer", "VLC Media Player" }, new List<int> { 0, 1 }));
-
+           
             hardCategory.AddQuestion(new QuizQuestion("1. Which of these are programming paradigms?\n", new List<string> { "Object-Oriented", "Functional", "Quantum", "Relational" }, new List<int> { 0, 1 }));
             hardCategory.AddQuestion(new QuizQuestion("2. Which of the following are renewable energy sources?\n", new List<string> { "Coal", "Solar", "Wind", "Natural Gas" }, new List<int> { 1, 2 }));
             hardCategory.AddQuestion(new QuizQuestion("3. Which two of the following languages are primarily used for backend web development?\n", new List<string> { "Ruby", "JavaScript", "PHP", "HTML" }, new List<int> { 0, 1 }));
             hardCategory.AddQuestion(new QuizQuestion("4. Which two of the following are features of object-oriented programming?\n", new List<string> { "Encapsulation", "Inheritance", "Pointers", "Recursion" }, new List<int> { 1, 2 }));
             hardCategory.AddQuestion(new QuizQuestion("5. Which of the following are commonly used version control systems?\n", new List<string> { "Git", "SVN", "Mercurial", "CVS" }, new List<int> { 0, 1 }));
-            hardCategory.AddQuestion(new QuizQuestion("6. Which two of the following are valid ways to declare a variable in JavaScript?\n", new List<string> { "let", "var", "const", "define" }, new List<int> { 0, 1 }));
-            hardCategory.AddQuestion(new QuizQuestion("7. Which two of the following are primitive data types in Java?\n", new List<string> { "String", "int", "double", "ArrayList" }, new List<int> { 1, 2 }));
-            hardCategory.AddQuestion(new QuizQuestion("8. Which of the following are used to handle exceptions in Python?\n", new List<string> { "try", "except", "catch", "finally" }, new List<int> { 0, 1 }));
-            hardCategory.AddQuestion(new QuizQuestion("9. Which two languages are primarily used for frontend web development?\n", new List<string> { "HTML", "CSS", "Python", "Java" }, new List<int> { 0, 1 }));
-            hardCategory.AddQuestion(new QuizQuestion("10 Which two data structures are most commonly used to implement a LRU cache?\n", new List<string> { "Stack", "Linked List", "HashMap", "Queue" }, new List<int> { 1, 2 }));
-
+           
             tooHardCategory.AddQuestion(new QuizQuestion("1. Which of these are units of time?\n", new List<string> { "Light-year", "Hour", "Meter", "Second" }, new List<int> { 1, 3 }));
             tooHardCategory.AddQuestion(new QuizQuestion("2. Which of these are types of economic systems?\n", new List<string> { "Communism", "Socialism", "Capitalism", "Nationalism" }, new List<int> { 0, 2 }));
             tooHardCategory.AddQuestion(new QuizQuestion("3. In object-oriented programming, which of the following are principles?\n", new List<string> { "Inheritance", "Polymorphism", "Reflection", "Abstraction" }, new List<int> { 0, 1 }));
             tooHardCategory.AddQuestion(new QuizQuestion("4. Which of the following are commonly used for version control?\n", new List<string> { "Git", "Subversion", "CVS", "Bitbucket" }, new List<int> { 0, 1 }));
             tooHardCategory.AddQuestion(new QuizQuestion("5. In Python, which of the following are valid string methods?\n", new List<string> { "append()", "split()", "join()", "pop()" }, new List<int> { 0, 2 }));
-            tooHardCategory.AddQuestion(new QuizQuestion("6. In C++, which of the following are access specifiers?\n", new List<string> { "public", "private", "protected", "final" }, new List<int> { 0, 1 }));
-            tooHardCategory.AddQuestion(new QuizQuestion("7. Which of the following are design patterns?\n", new List<string> { "Singleton", "Factory", "Encapsulation", "Inheritance" }, new List<int> { 0, 1 }));
-            tooHardCategory.AddQuestion(new QuizQuestion("8. Which of these are valid Python loops?\n", new List<string> { "for", "while", "until", "loop" }, new List<int> { 0, 1 }));
-            tooHardCategory.AddQuestion(new QuizQuestion("9. In SQL, which of the following are aggregate functions?\n", new List<string> { "SUM", "AVG", "SELECT", "INSERT" }, new List<int> { 0, 1 }));
-            tooHardCategory.AddQuestion(new QuizQuestion("10 Which of the following are functional programming languages?\n", new List<string> { "Haskell", "Lisp", "C++", "JavaScript" }, new List<int> { 0, 1 }));
+         
         }
  
         private void StartQuizMenu()
@@ -527,7 +491,7 @@ namespace QuizApp
                         Console.Clear();
                         return;
                 }
-                bool confirm = AskConfirmation("You are going to delete this category? (y/n): ");
+                bool confirm = AskConfirmation("You are going to delete this category? y/n: ");
                 if (confirm)
                 {
                     selectedCategory.DeleteAllQuestions();
@@ -640,7 +604,7 @@ namespace QuizApp
         {
             while (true)
             {
-                Console.Write("\nDo you want to make more? (y/n): ");
+                Console.Write("\nDo you want to make more? y/n: ");
                 string response = Console.ReadLine().Trim().ToLower();
                 Console.Clear();
 
